d5e1890bb97eecab9c870d90a8f42ba743146a2c
Brian <brianvm@gmail.com>
2025-04-10 10:59:54 -0500

    first commit
    Added boiler plate HTML, and JS functions for book creation.

---
+++ scritps.js
@@ -0,0 +1,27 @@
+const myLibrary = [];
+
+// BOOK CONSTRUCTOR
+function Book(title, author, pages, read) {
+    if (!new.target) {
+      throw Error("You must use the 'new' operator to call the constructor.");
+    }
+    
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+      const isRead = this.read ? "already read." : "not read yet."
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${isRead}`
+    };
+  }
+
+function addNewBook(title, author, pages, read) {
+    let newBook = new Book(title, author, pages, read);
+    newBook.id = crypto.randomUUID();
+    return myLibrary.push(newBook);
+}
+
+addNewBook("The Hobbit", "Tolkein", 1000, true);
+console.log(myLibrary);
+
