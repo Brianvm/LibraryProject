32b1ad03ee09100e5577f518aa611dd5bdef0bef
Brian <brianvm@gmail.com>
2025-04-14 15:54:53 -0500

    JS functionality
    Added some JS to make buttons functional and to show the data on the DOM. Working on an issue with visibility of the form.

--- scritps.js
+++ scritps.js
@@ -1,9 +1,41 @@
 const myLibrary = [];
 const cardMainContainer = document.querySelector("#card_main_container");
 const bookForm = document.querySelector("form");
+const bookTitle = document.querySelector('#title');
+const bookAuthor = document.querySelector('#author');
+const bookPages = document.querySelector('#pages');
+const readValue = document.querySelector('input[name="isRead"]:checked');
+
+// ADD BOOK BUTTON
 document.querySelector('#add_btn').addEventListener("click", () => {
+    console.log('Button clicked!')
     bookForm.style.visibility = "visible";
+    bookTitle.value = '';
+    bookAuthor.value = ''; 
+    bookPages.value = ''; 
+    readValue.value = '';
 });
+
+// FORM BUTTON
+document.querySelector('#form_btn').addEventListener('click', () => {
+    addNewBook(bookTitle.value, bookAuthor.value, bookPages.value, readValue.value);
+    bookTitle.value = '';
+    bookAuthor.value = ''; 
+    bookPages.value = ''; 
+    readValue.value = '';
+    bookForm.style.visibility = "hidden";
+
+    myLibrary.forEach(book => {
+        cardMainContainer.innerHTML += `
+        <article>
+            <h1>Title: ${book.title}</h1>
+            <p>Author: ${book.author}</p>
+            <p>Pages: ${book.pages}</p>
+            <p>Have you read it? ${book.read}</p>
+        </article>`
+    });
+});
+
 
 // BOOK CONSTRUCTOR
 function Book(title, author, pages, read) {
@@ -24,9 +56,8 @@ function Book(title, author, pages, read) {
 function addNewBook(title, author, pages, read) {
     let newBook = new Book(title, author, pages, read);
     newBook.id = crypto.randomUUID();
+    console.log(myLibrary);
     return myLibrary.push(newBook);
 }
 
-addNewBook("The Hobbit", "Tolkein", 1000, true);
-console.log(myLibrary);
 
