85f0ab8f9597bc48ebb2d35c22edb3dcf24953cc
Brian <brianvm@gmail.com>
2025-04-14 21:30:33 -0500

    Modified the JS
    Modified the JS to append a new child article instead of wiping and rerendering the entire list each time.

--- scritps.js
+++ scritps.js
@@ -15,15 +15,39 @@ document.querySelector('#form_btn').addEventListener('click', () => {
     const readValueElement = document.querySelector('input[name="isRead"]:checked');
     const readValueString = readValueElement ? readValueElement.value : "false"; // Default to "false" if none checked
     const readBoolean = readValueString === "true"; // Convert string to boolean
-    addNewBook(bookTitle.value, bookAuthor.value, bookPages.value, readBoolean);
+   const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, readBoolean);
+   addNewBook(newBook);
     bookTitle.value = '';
     bookAuthor.value = '';
     bookPages.value = ''; 
     bookForm.style.zIndex = -1;
 
-    cardMainContainer.innerHTML = "";
+    renderNewBook(newBook);
+});
 
-    myLibrary.forEach(book => {
+
+// BOOK CONSTRUCTOR
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor.");
+    }
+    
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function() {
+        const isRead = this.read ? "already read." : "not read yet."
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${isRead}`
+    };
+}
+
+function addNewBook(book) {
+    book.id = crypto.randomUUID();
+    return myLibrary.push(book);
+}
+
+function renderNewBook(book) {
         const article = document.createElement('article');
         article.innerHTML = `
         <h1>Title: ${book.title}</h1>
@@ -32,31 +56,5 @@ document.querySelector('#form_btn').addEventListener('click', () => {
         <p>Have you read it? ${book.read === true ? "Yes" : "No"}</p>
         `;
         cardMainContainer.appendChild(article);
-    });
-});
-
-
-// BOOK CONSTRUCTOR
-function Book(title, author, pages, read) {
-    if (!new.target) {
-      throw Error("You must use the 'new' operator to call the constructor.");
-    }
-    
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-      const isRead = this.read ? "already read." : "not read yet."
-      return `${this.title} by ${this.author}, ${this.pages} pages, ${isRead}`
-    };
-  }
-
-function addNewBook(title, author, pages, read) {
-    let newBook = new Book(title, author, pages, read);
-    newBook.id = crypto.randomUUID();
-    console.log(myLibrary);
-    return myLibrary.push(newBook);
 }
-
 
